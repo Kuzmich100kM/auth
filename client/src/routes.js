a37884d78bd35cc40c69b2dd433d742c35bf0992
Vasua Pupkin <webolololo@gmail.com>
2021-11-27 15:08:13 +0200

    Change Routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,9 +2,7 @@ import React from "react"
 import { useSelector } from "react-redux"
 import { Routes, Route, Navigate } from "react-router-dom"
 import Login from "./components/Auth/Login"
-import Login2 from "./components/Auth/Login2"
 import Registration from "./components/Auth/Registration"
-import Registration2 from "./components/Auth/Registration2"
 import OpenPage from "./components/OpenPage"
 import PrivatePage from "./components/PrivatePage"
 import Index from "./components/Index"
@@ -19,9 +17,7 @@ export default function routes() {
 		<Routes>
 			<Route exact path="/" element={<Index />} />
 			<Route exact path="login" element={<Login />} />
-			<Route exact path="login2" element={<Login2 />} />
 			<Route exact path="registration" element={<Registration />} />
-			<Route exact path="registration2" element={<Registration2 />} />
 			<Route path="openpage" element={<OpenPage />} />
 
 			<Route
@@ -35,29 +31,3 @@ export default function routes() {
 		</Routes>
 	)
 }
-
-// function PrivateRoute({ child, ...rest }) {
-// 	const inProgress = useSelector(state => state.user)
-// 	const isAuth = useSelector(state => state.user)
-
-// 	// Double this block with <Header/> component
-// 	// const dispatch = useDispatch()
-// 	// useEffect(() => {
-// 	// 	dispatch(refreshUserAuthTC())
-// 	// }, [dispatch])
-
-// 	return (
-// 		<Route
-// 			{...rest}
-// 			render={({ location }) =>
-// 				isAuth ? child : <Navigate to={{ pathname: "/auth/login", state: { from: location } }} />
-// 			}
-// 		/>
-// 	)
-// }
-
-/* <Route exact path="/strats/add" render={() => <AddFormStratContainer />} />
-<Route exact path="/strats/:stratId" render={() => <EditFormStratContainer />} /> 
-<Route path="/trade" render={() => <Trades />} />
-<Route path="/orders" render={() => <OrdersContainer />} />
-<Route path="/positions" render={() => <PositionsContainer />} /> */
